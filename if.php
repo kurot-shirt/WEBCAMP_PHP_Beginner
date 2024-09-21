e9450170b3d929a4b48830b1544baca5d8a441c3
aiko <aiko@example.com>
2024-09-21 09:46:43 +0000

    if.phpを書いた

---
+++ if.php
@@ -0,0 +1,27 @@
+<?php
+
+if (true) {
+    echo "ture だとここを通る? \n";
+}
+if (false) {
+    echo "false だとここを通る? \n";
+}
+$age = 20;
+if ($age >= 20) {
+    echo "成人です \n";
+}
+if ($age < 20) {
+    echo "未成年です \n";
+}
+if ($age >= 20) {
+    echo "成人です \n";
+} else {
+    echo "未成年です \n";
+}    
+if ($age > 20) {
+    echo "成人 \n";
+} else if ($age === 20) {
+    echo "新成人おめでとう！！ \n";
+}else {
+    echo "未成年 \n";
+}
